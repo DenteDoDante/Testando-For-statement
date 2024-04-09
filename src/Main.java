fe5db9aace9cb03aa524b8704f5161de242086ae
Joilson <richarddsmaia@gmail.com>
2024-04-09 11:22:59 -0300

    For feito

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        var repet = 2;
+        for ( var i = 0; i < 50; repet ++) {
+            System.out.println(i);
+            repet = repet *2;
+            i = repet;
+
+        }
+
+
+    }
+}
